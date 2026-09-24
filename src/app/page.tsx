844927551a73c4f1f04edadf3d75723b3551e580
Agda Lopes <domingoslopesagda@gmail.com>
2026-09-24 16:01:37 -0100

    Update home page styles to enhance readability with a white background and black text.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <main className="flex min-h-svh items-center justify-center">
+    <main className="flex min-h-svh items-center justify-center bg-white text-black">
       <h1 className="text-4xl">Olá</h1>
     </main>
   );
